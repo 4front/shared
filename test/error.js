cfd7b9796aa8f170fdcad42723ee532dc35c5ec4
David Von Lehman <david.vonlehman@gmail.com>
2015-12-03 17:02:54 -0800

    Fixing error middleware test

--- test/error.js
+++ test/error.js
@@ -1,5 +1,6 @@
 // import assert from 'assert';
 var express = require('express');
+var sinon = require('sinon');
 var supertest = require('supertest');
 var errorMiddleware = require('../lib/middleware/error');
 
@@ -13,17 +14,28 @@ describe('error middleware', function() {
   beforeEach(function() {
     app = express();
 
+    app.settings.logger = {
+      middleware: {
+        error: sinon.spy(function(err, req, res, next) {
+          next();
+        })
+      }
+    };
+
     error = new Error('error');
     app.use(function(req, res, next) {
       next(error);
     });
 
-    app.use(errorMiddleware());
+    app.use(errorMiddleware(app.settings));
   });
 
   it('does something', function(done) {
     supertest(app).get('/')
       .expect(500)
+      .expect(function(res) {
+        app.settings.logger.middleware.error.calledWith(sinon.match({status: 500}));
+      })
       .end(done);
   });
 
