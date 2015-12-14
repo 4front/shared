4941b126d668bd6888134cfc70972d9de07a3f69
David Von Lehman <david.vonlehman@gmail.com>
2015-12-14 13:09:09 -0800

    Adding setting-defaults

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ exports.crypto = require('./lib/crypto');
 exports.configure = require('./lib/configure');
 
 exports.routes = {
+  crypto: require('./lib/crypto'),
   debug: require('./lib/middleware/debug'),
   error: require('./lib/middleware/error'),
   healthCheck: require('./lib/middleware/health'),
