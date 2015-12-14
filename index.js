95a9bb6d97bea7cd3d6494d21ddd1b239c9a1b41
David Von Lehman <david.vonlehman@gmail.com>
2015-12-14 13:28:31 -0800

    export settingDefaults

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ exports.routes = {
   debug: require('./lib/middleware/debug'),
   error: require('./lib/middleware/error'),
   healthCheck: require('./lib/middleware/health'),
-  catchAll: require('./lib/middleware/catch-all')
+  catchAll: require('./lib/middleware/catch-all'),
+  settingDefaults: require('./lib/setting-defaults')
 };
