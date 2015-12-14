b072a9e5d98b1e0a05b4971b7c171679a9845289
David Von Lehman <david.vonlehman@gmail.com>
2015-12-14 13:32:22 -0800

    Export settingDefaults correctly

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 exports.crypto = require('./lib/crypto');
 exports.configure = require('./lib/configure');
+exports.settingDefaults = require('./lib/setting-defaults');
 
 exports.routes = {
-  crypto: require('./lib/crypto'),
   debug: require('./lib/middleware/debug'),
   error: require('./lib/middleware/error'),
   healthCheck: require('./lib/middleware/health'),
-  catchAll: require('./lib/middleware/catch-all'),
-  settingDefaults: require('./lib/setting-defaults')
+  catchAll: require('./lib/middleware/catch-all')
 };
